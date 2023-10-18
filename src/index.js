4cd9d2f8e433add928f8caadc5732b2bfb16c941
adamcyberspace.com <80759397+AmtelICT@users.noreply.github.com>
2023-10-18 06:32:13 +0300

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import {
 
 import './style.css'
 import Allset from './views/allset'
-import Login from './views/login'
-import Splash from './views/splash'
-import Register from './views/register'
 import Onboard1 from './views/onboard1'
-import Home from './views/home'
-import Onboard2 from './views/onboard2'
-import Onboard3 from './views/onboard3'
-import Onboard4 from './views/onboard4'
 import Onboard5 from './views/onboard5'
+import Onboard4 from './views/onboard4'
+import Splash from './views/splash'
+import Login from './views/login'
+import Home from './views/home'
+import Onboard3 from './views/onboard3'
+import Register from './views/register'
+import Onboard2 from './views/onboard2'
 import NotFound from './views/not-found'
 
 const App = () => {
@@ -25,15 +25,15 @@ const App = () => {
     <Router>
       <Switch>
         <Route component={Allset} exact path="/allset" />
-        <Route component={Login} exact path="/sign-in1" />
-        <Route component={Splash} exact path="/" />
-        <Route component={Register} exact path="/register" />
         <Route component={Onboard1} exact path="/onboard1" />
-        <Route component={Home} exact path="/home" />
-        <Route component={Onboard2} exact path="/onboard2" />
-        <Route component={Onboard3} exact path="/onboard3" />
-        <Route component={Onboard4} exact path="/onboard4" />
         <Route component={Onboard5} exact path="/onboard5" />
+        <Route component={Onboard4} exact path="/onboard4" />
+        <Route component={Splash} exact path="/" />
+        <Route component={Login} exact path="/sign-in1" />
+        <Route component={Home} exact path="/home" />
+        <Route component={Onboard3} exact path="/onboard3" />
+        <Route component={Register} exact path="/register" />
+        <Route component={Onboard2} exact path="/onboard2" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
